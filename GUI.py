253c66143745ae1e29dbd12fc54435671c27690f
owcap <owcap@LAPTOP-P66KU583>
2022-01-10 22:21:53 +0700

    pos done

--- GUI.py
+++ GUI.py
@@ -1,10 +1,8 @@
 from tkinter import * 
-import tkinter.scrolledtext as scrolledtext
 from tkinter.ttk import *
-from tkinter.filedialog import askopenfilename
 import HMM as hm
-import pandas as pd
-import re
+import CRF as cr
+import spacy
 
 class HMM:
     def __init__(self, master):
@@ -19,7 +17,7 @@ class HMM:
         self.statusbar = Label(self.tab, textvariable=self.status , relief=SUNKEN, anchor=W, state=DISABLED)
         self.statusbar.pack(side=BOTTOM, fill=X)
 
-        self.label = Label(self.tab, text="Viterbi Part-of-speech Tagger",width=30,font=("bold", 11))
+        self.label = Label(self.tab, text="Hidden Markov Model",width=30,font=("bold", 11))
         self.label.place(x=250,y=10)
    
         self.input = Text(self.tab, height=10, width=80)
@@ -42,13 +40,86 @@ class HMM:
         inputValue=self.input.get("1.0",END).splitlines()
         self.output.configure(state='normal')
         self.output.delete("1.0","end")
-        for idx, i in enumerate(inputValue):
-            tmp = re.findall(r"'\w+|n't|\w+(?=n't)|\w+|[^\s\w]", i)
+        for sent in inputValue:
+            tmp = nlp(sent)
+            tmp = [token.text for token in tmp]
             seq = hm.Viterbi(tmp, self.tags_df, self.train_tagged_words)
             self.output.insert(END, seq)
             self.output.insert(END, '\n')
 
 
+class CRF:
+    def __init__(self, master):
+        self.master = master
+        self.tab = Frame(self.master)
+        self.master.add(self.tab, text ='CRF')
+
+        #Load the transition matrix from training file
+        self.model = self.load_model()
+        self.status = StringVar(self.tab)
+        self.status.set('Model loaded sucessfully!')
+        self.statusbar = Label(self.tab, textvariable=self.status , relief=SUNKEN, anchor=W, state=DISABLED)
+        self.statusbar.pack(side=BOTTOM, fill=X)
+
+        self.label = Label(self.tab, text="Conditional Random Field",width=30,font=("bold", 11))
+        self.label.place(x=250,y=10)
+   
+        self.input = Text(self.tab, height=10, width=80)
+        self.input.place(x=30, y=35)
+
+        self.test = Button(self.tab, text='Test', command = self.tag)
+        self.test.place(x=300,y=211)
+
+        self.output = Text(self.tab, height=10, width=80)
+        self.output.insert(INSERT, 'Waiting for input...')
+        self.output.configure(state='disabled')
+        self.output.place(x=30, y=250)
+
+        
+    def load_model(self):
+        model = cr.load_model('crf.joblib')
+        return model
+
+    def feature_extract(self, sent, idx):
+        word = sent[idx]
+        if idx == 0:
+            first = True
+        else:
+            first = False
+        if idx == len(sent) - 1:
+            last = True
+        else:
+            last = False
+
+        features = {
+            'word' : word,
+            'word.lower()': word.lower(),
+            'number': word.isdigit(),
+            'word.istitle()': word.istitle(),
+            'word.isupper()': word.isupper(),
+            'has_hyphen': '-' in word,
+            'is_first': first,
+            'is_last': last
+        }
+        return features
+
+    def tag(self):
+        inputValue=self.input.get("1.0",END).splitlines()
+        self.output.configure(state='normal')
+        self.output.delete("1.0","end")
+        for sent in inputValue:
+            tmp = nlp(sent)
+            tmp = [token.text for token in tmp]
+            # print(tmp)
+            features = [self.feature_extract(tmp, idx) for idx, i in enumerate(tmp)]
+            seq = self.model.predict([features])[0]
+            # print(seq)
+            out = list(zip(tmp, seq))
+            # print(out)
+            self.output.insert(END, out)
+            self.output.insert(END, '\n')
+
+nlp = spacy.load('en_core_web_sm', disable=['ner', 'parser'])
 
 root = Tk()
 root.geometry('700x500') 
@@ -56,6 +127,7 @@ root.title("POS Tagger")
 tabControl = Notebook(root)
   
 hmm = HMM(tabControl)
+crf = CRF(tabControl)
 
 tabControl.pack(expand = 1, fill ="both")
 
